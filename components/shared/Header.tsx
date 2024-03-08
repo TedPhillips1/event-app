bd26f71bacc139d802a41791b41fd2e5bc741d25
ted.phillips <ted.phillips11@btinternet.com>
2024-03-08 16:52:52 +0000

    Clerk partially added

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,7 +1,31 @@
+import { SignedOut } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import { Button } from "../ui/button";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className='w-full border-b'>
+      <div className='wrapper flex items-center justify-between'>
+        <Link href='/'>
+          <Image
+            src='/assets/images/logo.svg'
+            width={128}
+            height={38}
+            alt='event app logo'
+          />
+        </Link>
+        <div className='flex w-32 justify-end gap-3'>
+          <SignedOut>
+            <Button asChild className='rounded-full' size='lg'>
+              <Link href='/sign-in'>Login</Link>
+            </Button>
+          </SignedOut>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
