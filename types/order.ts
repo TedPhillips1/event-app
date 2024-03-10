3a4db2214a72081e9fa1e7697e3b8d8ce793cc86
TedPhillips1 <ted.phillips11@btinternet.com>
2024-03-10 13:49:35 +0000

    database and clerk setup

---
+++ types/order.ts
@@ -0,0 +1,26 @@
+export type CheckoutOrderParams = {
+    eventTitle: string;
+    eventId: string;
+    price: string;
+    isFree: boolean;
+    buyerId: string;
+};
+
+export type CreateOrderParams = {
+    stripeId: string;
+    eventId: string;
+    buyerId: string;
+    totalAmount: string;
+    createdAt: Date;
+};
+
+export type GetOrdersByEventParams = {
+    eventId: string;
+    searchString: string;
+};
+
+export type GetOrdersByUserParams = {
+    userId: string | null;
+    limit?: number;
+    page: string | number | null;
+};
