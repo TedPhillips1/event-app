b7837e5ced44699b1285ddd6c6a575711178be21
ted.phillips <ted.phillips11@btinternet.com>
2024-03-18 22:56:18 +0000

    added collection

---
+++ components/shared/DeleteConfirmation.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { useTransition } from "react";
+import { usePathname } from "next/navigation";
+import Image from "next/image";
+
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "../ui/button";
+
+import { deleteEvent } from "@/lib/database/actions/event.actions";
+
+export const DeleteConfirmation = ({ eventId }: { eventId: string }) => {
+  const pathname = usePathname();
+  let [isPending, startTransition] = useTransition();
+
+  return (
+    <Dialog>
+      <DialogTrigger>
+        <Image
+          src='/assets/icons/delete.svg'
+          alt='edit'
+          width={24}
+          height={24}
+        />
+      </DialogTrigger>
+
+      <DialogContent className='bg-white'>
+        <DialogHeader>
+          <DialogTitle>Are you sure you want to delete?</DialogTitle>
+          <DialogDescription>
+            This will permanently delete this event
+          </DialogDescription>
+        </DialogHeader>
+
+        <DialogFooter>
+          <DialogClose>
+            <Button variant='secondary'>Cancel</Button>
+          </DialogClose>
+          <DialogClose>
+            <Button
+              variant='destructive'
+              type='submit'
+              onClick={() =>
+                startTransition(async () => {
+                  await deleteEvent({ eventId, path: pathname });
+                })
+              }>
+              {isPending ? "Deleting..." : "Delete"}
+            </Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
--- types/event.ts
+++ types/event.ts
@@ -1,81 +1,84 @@
+import { IEvent } from "@/lib/database/models/event.model";
+
 export type CreateEventParams = {
-    userId: string;
-    event: {
-        title: string;
-        description: string;
-        location: string;
-        imageUrl: string;
-        startDateTime: Date;
-        endDateTime: Date;
-        categoryId: string;
-        price: string;
-        isFree: boolean;
-        url: string;
-    };
-    path: string;
+  userId: string;
+  event: {
+    title: string;
+    description: string;
+    location: string;
+    imageUrl: string;
+    startDateTime: Date;
+    endDateTime: Date;
+    categoryId: string;
+    price: string;
+    isFree: boolean;
+    url: string;
+  };
+  path: string;
 };
 
 export type UpdateEventParams = {
-    userId: string;
-    event: {
-        _id: string;
-        title: string;
-        imageUrl: string;
-        description: string;
-        location: string;
-        startDateTime: Date;
-        endDateTime: Date;
-        categoryId: string;
-        price: string;
-        isFree: boolean;
-        url: string;
-    };
-    path: string;
-};
-
-export type DeleteEventParams = {
-    eventId: string;
-    path: string;
-};
-
-export type GetAllEventsParams = {
-    query: string;
-    category: string;
-    limit: number;
-    page: number;
-};
-
-export type GetEventsByUserParams = {
-    userId: string;
-    limit?: number;
-    page: number;
-};
-
-export type GetRelatedEventsByCategoryParams = {
-    categoryId: string;
-    eventId: string;
-    limit?: number;
-    page: number | string;
-};
-
-export type Event = {
+  userId: string;
+  event: {
     _id: string;
     title: string;
-    description: string;
-    price: string;
-    isFree: boolean;
     imageUrl: string;
+    description: string;
     location: string;
     startDateTime: Date;
     endDateTime: Date;
+    categoryId: string;
+    price: string;
+    isFree: boolean;
     url: string;
-    organizer: {
-        _id: string;
-        firstName: string;
-        lastName: string;
-    };
-    category: {
-        _id: string;
-        name: string;
-    };
+  };
+  path: string;
 };
+
+export type DeleteEventParams = {
+  eventId: string;
+  path: string;
+};
+
+export type GetAllEventsParams = {
+  query: string;
+  category: string;
+  limit: number;
+  page: number;
+};
+
+export type GetEventsByUserParams = {
+  userId: string;
+  limit?: number;
+  page: number;
+};
+
+export type GetRelatedEventsByCategoryParams = {
+  categoryId: string;
+  eventId: string;
+  limit?: number;
+  page: number | string;
+};
+
+export type Event = {
+  _id: string;
+  title: string;
+  description: string;
+  price: string;
+  isFree: boolean;
+  imageUrl: string;
+  location: string;
+  startDateTime: Date;
+  endDateTime: Date;
+  url: string;
+  organizer: {
+    _id: string;
+    firstName: string;
+    lastName: string;
+  };
+  category: {
+    _id: string;
+    name: string;
+  };
+};
+
